4b9597b9a04fff43e3ca21048d06335a3be8cf09
Jose Luis Ponton <45894435+JLPM22@users.noreply.github.com>
2022-07-11 23:02:27 +0300

    init test

--- bin/dockerhub_run.sh
+++ bin/dockerhub_run.sh
@@ -2,6 +2,6 @@ FILE=Gemfile.lock
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
-docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
+winpty docker run --rm -v "C:\Users\jlpon\Desktop\Web\JLPM22.github.io:/srv/jekyll/" -p "8080:8080" \
                     -it amirpourmand/al-folio bundler  \
                     exec jekyll serve --watch --port=8080 --host=0.0.0.0 
